b1892fbd01f0f194805687d7e4a44f575a8d75ba
bosees <rladuddlf3@naver.com>
2021-09-29 10:24:17 +0900

    해쉬테이블 충돌 개선, 체이닝기법

--- python_basic1/hashtable.js
+++ python_basic1/hashtable.js
@@ -1,20 +1,40 @@
 const hashTable = Array.from({length:8}, () => 0);
 
 function hashFunction(key) {
-  let hashAddress = key.charCodeAt([0]);
-  return hashAddress % 8;
+  return key % 8;
 }
 
 function add(key, value) {
-  const hashAddress = hashFunction(key);
-  hashTable[hashAddress] = value;
+  let indexKey = key.charCodeAt([0]);
+  const hashAddress = hashFunction(indexKey);
+
+  if (hashTable[hashAddress] === 0) {
+    hashTable[hashAddress] = [[indexKey, value]];
+  } else {
+    hashTable[hashAddress].push([indexKey,value]);
+  }
 }
 
 function find(key) {
-  const hashAddress = hashFunction(key);
-  return hashTable[hashAddress];
+  const indexKey = key.charCodeAt([0]);
+  const hashAddress = hashFunction(indexKey);
+
+  if (hashTable[hashAddress] === 0) {
+    return "데이터가 없습니다."
+  } else {
+    for (let i of hashTable[hashAddress]) {
+      if(i[0] === indexKey) {
+        return i[1];
+      }
+    }
+  }
 }
 
-console.log(add("duddlf",123123123));
+console.log(add("duddlf",1111111));
+console.log(add("dbdlf",2222222));
+console.log(add("zero",2222222));
+console.log(add("nomen",3333333));
+console.log(hashTable);
 
-console.log(find("duddlf"));
+console.log(find("dbdlf"))
+

380d8da75cd7de20a6d82aaf6ed4de956065a165
c <rladuddlf3@naver.com>
2021-10-15 23:43:25 +0900

    제로베이스 자료구조

--- python_basic1/hashtable.js
+++ python_basic1/hashtable.js
@@ -1,5 +1,48 @@
 
-// 체이닝 기법 - 해쉬테이블 & 링크드리스트(배열)
+//체이닝 기법 - 해쉬테이블 & 링크드리스트(배열)
+const hashTable = Array.from({length:8}, () => 0);
+function hashFunction(key) {
+  return key % 8;
+}
+
+function set(key, value) {
+  let indexKey = key.charCodeAt([0]);
+  const hashAddress = hashFunction(indexKey);
+
+  if (hashTable[hashAddress] === 0) {
+    hashTable[hashAddress] = [[indexKey, value]];
+  } else {
+    hashTable[hashAddress].push([indexKey,value]);
+  }
+}
+
+function get(key) {
+  const indexKey = key.charCodeAt([0]);
+  const hashAddress = hashFunction(indexKey);
+
+  if (hashTable[hashAddress] === 0) {
+    return "데이터가 없습니다."
+  } else {
+    for (let i of hashTable[hashAddress]) {
+      if(i[0] === indexKey) {
+        return i[1];
+      }
+    }
+  }
+}
+
+console.log(set("hello",1111111));
+console.log(set("duddlf2",2222222));
+console.log(set("zero3",2222222));
+console.log(set("base4",3333333));
+
+console.log(get("hello"));
+console.log(get("duddlf2"));
+console.log(get("zero3"));
+console.log(get("base4"));
+
+
+//폐쇠기법 - 리니어 프로브
 // const hashTable = Array.from({length:8}, () => 0);
 // function hashFunction(key) {
 //   return key % 8;
@@ -8,7 +51,6 @@
 // function add(key, value) {
 //   let indexKey = key.charCodeAt([0]);
 //   const hashAddress = hashFunction(indexKey);
-
 //   if (hashTable[hashAddress] === 0) {
 //     hashTable[hashAddress] = [[indexKey, value]];
 //   } else {
@@ -38,44 +80,4 @@
 // console.log(hashTable);
 
 // console.log(find("dbdlf"))
-
-
-//폐쇠기법 - 리니어 프로브
-const hashTable = Array.from({length:8}, () => 0);
-function hashFunction(key) {
-  return key % 8;
-}
-
-function add(key, value) {
-  let indexKey = key.charCodeAt([0]);
-  const hashAddress = hashFunction(indexKey);
-  if (hashTable[hashAddress] === 0) {
-    hashTable[hashAddress] = [[indexKey, value]];
-  } else {
-    hashTable[hashAddress].push([indexKey,value]);
-  }
-}
-
-function find(key) {
-  const indexKey = key.charCodeAt([0]);
-  const hashAddress = hashFunction(indexKey);
-
-  if (hashTable[hashAddress] === 0) {
-    return "데이터가 없습니다."
-  } else {
-    for (let i of hashTable[hashAddress]) {
-      if(i[0] === indexKey) {
-        return i[1];
-      }
-    }
-  }
-}
-
-console.log(add("duddlf",1111111));
-console.log(add("dbdlf",2222222));
-console.log(add("zero",2222222));
-console.log(add("nomen",3333333));
-console.log(hashTable);
-
-console.log(find("dbdlf"))
 
--- python_basic1/linkedlist.js
+++ python_basic1/linkedlist.js
@@ -1,45 +1,105 @@
-// 연결리스트 구현
+  // // 연결리스트 구현
 
-function LinkedList() {
-  this.head = null;
-  this.length = 0;
+// function LinkedList() {
+//   this.head = null;
+//   this.length = 0;
 
-  this.add = (data) => {
-    const node = new Node(data);
-    if(this.head === null) {
-      this.head = node;
-      this.length++;
-    } else {
-      const prov = this.head;
-      this.head = node;
-      this.head.next = prov;
-      this.length++;
-    }
+//   this.add = (data) => {
+//     const node = new Node(data);
+//     if(this.head === null) {
+//       this.head = node;
+//       this.length++;
+//     } else {
+//       const prov = this.head;
+//       this.head = node;
+//       this.head.next = prov;
+//       this.length++;
+//     }
+//   }
+// }
+
+// function Node(data) {
+//   this.data = data;
+//   this.next = null
+// }
+
+// let linkedList = new LinkedList();
+// console.log(linkedList.this);
+
+// const sol = () => {
+//   return this
+// }
+// function sol2() {
+//   return this
+// }
+
+// console.log(sol()); 
+// console.log(sol2()); 
+
+
+
+// let name1 = "duddlf";
+// let name2 = name1;
+// name1 = "wpfkem";
+
+// console.log(name1, name2);
+
+class Node {
+  constructor(data, prev = null, next = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
   }
 }
 
-function Node(data) {
-  this.data = data;
-  this.next = null
+class LinkedQueue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
+
+  is_empty() {
+    if(this.front === null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  put(data) {
+    const node = new Node(data);
+
+    if (this.front === null) {
+      this.front = node;
+      this.rear = node;
+    } else {
+      let prevNode = this.rear;
+      prevNode.next = node;
+      this.rear = node;
+      this.rear.prev = prevNode;
+    }
+  }
+
+  get() {
+    let front = this.front;
+
+    if (front === null) return null;
+    else {
+      this.front = front.next;
+      this.front.prev = null;
+    }
+    return front.data;
+  }
+
+  peek() {
+    if (this.front === null) return null;
+    else return this.front.data;
+  }
+
 }
 
-let linkedList = new LinkedList();
-console.log(linkedList.this);
+let test = new LinkedQueue();
 
-const sol = () => {
-  return this
-}
-function sol2() {
-  return this
-}
-
-console.log(sol()); 
-console.log(sol2()); 
-
-
-
-let name1 = "duddlf";
-let name2 = name1;
-name1 = "wpfkem";
-
-console.log(name1, name2);
+console.log(test.put("aa"));
+console.log(test.peek());
+console.log(test)
